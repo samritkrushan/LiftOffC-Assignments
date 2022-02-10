825486e993972edde828342c15f1a1c057b21fd9
samritkrushan <99341611+samritkrushan@users.noreply.github.com>
2022-02-11 01:06:35 +0530

    Update locassignment5.c

--- locassignment5.c
+++ locassignment5.c
@@ -5,8 +5,8 @@ int main()
     printf("enter value for radius  :   ");
     scanf("%f" ,&r);
     d=2*r;
-    c=2*(22/7)*r;
-    area=(22/7)*r*r;
+    c=2*(22.0/7.0)*r;
+    area=(22.0/7.0)*r*r;
     printf("the diameter of circle is  :    %f" ,d);
     printf("the cicumference of the circle is   :    %f" ,c);
     printf("the area of circle is   :  %f  " ,area );
