98dbecae0e33a47037582a5885ec69b6188442cc
samritkrushan <99341611+samritkrushan@users.noreply.github.com>
2022-02-11 22:59:49 +0530

    Update and rename locassignment4.c to week1_Q4.c

--- week1_Q4.c
+++ week1_Q4.c
@@ -2,6 +2,7 @@
 int main()
 {
     int a,b,c,x;
+    float m;
     printf("If you want addition choose ...1...  \n  If you want subtraction choose ...2...  \n  If you want multiplication choose ...3...  \n  If you want division choose ...4...  \n   If you want remainder choose ...5...  \n");
     scanf("%d",&c);
     printf("Enter two numbers you want to do the operation");
@@ -21,8 +22,8 @@ int main()
             printf("the product of the numbers is  : %d   " ,x);
             break;
         case 4:
-            x=a/b;
-            printf("the quotient of the numbeers is  : %d  " ,x);
+            m=a/(float)b;
+            printf("the quotient of the numbeers is  : %f  " ,m);
             break;
         case 5:
             x=a%b;
